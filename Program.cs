b8481fd371bd056aa09a327fc07c9cd606142a06
Dell <Dell@DESKTOP-O7TQ14S>
2024-02-24 15:16:18 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
